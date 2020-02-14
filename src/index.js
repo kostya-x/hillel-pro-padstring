416f5037552112b7cb1684ff157d57fd90576980
Constantine <constantine-x@outlook.com>
2020-02-14 22:05:07 +0200

    user trinary operator

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 function padString(string, number, symbol, flag = true) {
   let newString = '';
+  let symbols = '';
 
   function addSymbols() {
     if (string.length < number) {
       for (let i = 0; i < number - string.length; i++) {
-        newString += symbol;
+        symbols += symbol;
       }
     }
   }
 
-  if (flag) {
-    newString = string;
-    addSymbols();
-  } else {
-    addSymbols();
-    newString += string;
-  }
+  addSymbols();
+
+  flag ? (newString = string + symbols) : (newString = symbols + string);
 
   return newString;
 }
